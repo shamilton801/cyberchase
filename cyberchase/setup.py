8765ce8dd6a17f96beef8f1cd865ba1b65b18646
TBolton2000 <TBolton2000@aol.com>
2022-02-19 13:06:04 -0600

    change version number

--- cyberchase/setup.py
+++ cyberchase/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cyberchase',
-    version='0.1.13',
+    version='1.0.0',
     description='Game code for TAMU Spring 2022 Turing Games',
     url='https://github.com/smh3005/cyberchase',
     author='Seth Hamilton and Trevor Bolton',
